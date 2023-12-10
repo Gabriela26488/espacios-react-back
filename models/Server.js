6b49fb8114f4424929d51632925a5a68ca368792
Gabriela26488 <patriciaf0907@gmail.com>
2023-12-09 22:44:30 -0400

    configuraciones iniciales, conexion a base de datos, modelo server que controla la iniciacion del sistema, configuracion de passport y JWT, modelo de usuario

---
+++ models/Server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const passportMiddleware = require("../middlewares/passport");
+
+const dbConnection = require("../db/config.db");
+const crearAdmin = require("../libs/initialSetup");
+
+// clase principal para daministrar el sistema
+class Server {
+  constructor() {
+    this.app = express();
+
+    this.port = process.env.PORT;
+
+    this.path = {
+      auth: "/api/auth",
+      usuarios: "/api/usuarios",
+    };
+
+    this.dataBase();
+
+    this.middlewares();
+    
+    this.initialSetup();
+    
+    this.listen();
+
+  }
+
+  async dataBase() {
+    await dbConnection();
+  }
+
+  // Metodo que establece los middlewares
+  middlewares() {
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.static("public"));
+    passport.use(passportMiddleware);
+  }
+
+  // Metodo que establece las configuraciones iniciales
+  async initialSetup() {
+    await crearAdmin();
+  }
+
+  // Metodo que carga las rutas
+  routes() {
+    
+  }
+
+  // metodo que arranca el servidor
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("corriendo en el puerto: " + this.port);
+    });
+  }
+}
+
+module.exports = Server;
